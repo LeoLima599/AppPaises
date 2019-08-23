2a9bbe4aeb9d2534ab713aac3d727608dc712349
Felipe Carneiro <felipefalconwolf@hotmail.com>
2019-08-23 15:05:23 -0300

    Initial commit

---
+++ app/src/main/java/br/com/bossini/apppaises/MainActivity.java
@@ -0,0 +1,16 @@
+package br.com.bossini.apppaises;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.media.MediaDataSource;
+import android.os.Bundle;
+import android.widget.EditText;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
